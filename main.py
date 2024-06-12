3a382a735634ad62927897927ea2429c4788ab61
lilian.noacco <lilian@noacco.fr>
2024-06-12 15:57:38 +0200

    feat(font):html + css + static folder | feat(back): flight date formatting

--- main.py
+++ main.py
@@ -1,23 +1,38 @@
-# app.py
 from flask import Flask, jsonify, request, render_template
 import requests
+from datetime import datetime
+from babel.dates import format_datetime
 
 app = Flask(__name__)
 
 # Configuration de l'URL de base de l'API DRF
-API_BASE_URL = "http://127.0.0.1:8000/"  # Changez cela en fonction de l'URL de votre API DRF
+API_BASE_URL = "http://127.0.0.1:8000/"
 
 @app.route('/')
 def index():
     return "Bienvenue à l'application Flask client pour l'API DRF"
+
+def format_date(date_str):
+    try:
+        date_obj = datetime.fromisoformat(date_str)
+        return format_datetime(date_obj, 'EEEE d MMMM yyyy \'à\' HH\'h\'mm', locale='fr_FR')
+    except ValueError as e:
+        # Gestion des erreurs de format de date
+        print(f"Erreur de formatage de date: {e}")
+        return date_str
 
 @app.route('/vols')
 def get_vols():
     response = requests.get(f"{API_BASE_URL}api/")
     print(response)
     vols = response.json()
-    return render_template('vols.html', vols=vols)
 
+    # Formater les dates
+    for vol in vols:
+        vol['vol_date_depart'] = format_date(vol['vol_date_depart'])
+        vol['vol_date_arrive'] = format_date(vol['vol_date_arrive'])
+
+    return render_template('vols.html', vols=vols)
 
 @app.route('/achats')
 def get_achats():
