091b2532c443e7155e32dc7a5792a5839f4dd745
Dorian P <poellen.dorian@gmail.com>
2024-06-12 11:24:31 +0200

    Ajout enregistrement des users

--- sae410/compagnie/urls.py
+++ sae410/compagnie/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import vol_list, vol_detail, reservation_detail, reservation_list, achat_list, achat_detail
+from .views import vol_list, vol_detail, reservation_detail, reservation_list, achat_list, achat_detail, register
 
 urlpatterns = [
     path('api/', vol_list),
     path ('api/vol/<int:id>/', vol_detail),
     path ('api/reservation/', reservation_list),
     path ('api/reservation/<int:id>/', reservation_detail),
+    path ('api/achat/', achat_list),
+    path ('api/achat/<int:id>/', achat_detail),
+    path ('api/register/', register),
 ]
--- sae410/compagnie/views.py
+++ sae410/compagnie/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Vol, Reservation, Achat
-from .serializer import AchatSerializer, VolSerializer, ReservationSerializer
+from .serializer import AchatSerializer, VolSerializer, ReservationSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
 import json
@@ -130,3 +130,12 @@ def achat_detail(request, pk):
     elif request.method == 'DELETE':
         achat.delete()
         return HttpResponse(status=204)
+
+@api_view(['POST'])
+def register(request):
+    if request.method == 'POST':
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
