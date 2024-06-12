ca8384bf5162d7abe8e59d4e638caaf40fe2200d
Dorian P <poellen.dorian@gmail.com>
2024-06-12 14:10:17 +0200

    Ajout de l'authentification

--- sae410/compagnie/urls.py
+++ sae410/compagnie/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import vol_list, vol_detail, reservation_detail, reservation_list, achat_list, achat_detail, register
+from .views import vol_list, vol_detail, reservation_detail, reservation_list, achat_list, achat_detail, register, login, test_token
 
 urlpatterns = [
     path('api/', vol_list),
@@ -9,4 +9,6 @@ urlpatterns = [
     path ('api/achat/', achat_list),
     path ('api/achat/<int:id>/', achat_detail),
     path ('api/register/', register),
+    path ('api/login/', login),
+    path ('api/test_login/', test_token),
 ]
--- sae410/compagnie/views.py
+++ sae410/compagnie/views.py
@@ -1,12 +1,17 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from django.http import HttpResponse, JsonResponse
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Vol, Reservation, Achat
 from .serializer import AchatSerializer, VolSerializer, ReservationSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 import json
 @csrf_exempt
 @api_view(['GET','POST'])
@@ -137,5 +142,24 @@ def register(request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = User.objects.get(username=request.data['username'])
+            user.save()
+            token = Token.objects.create(user=user)
+            return Response({'token': token.key, 'user': serializer.data})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response("missing user", status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(user)
+    return Response({'token': token.key, 'user': serializer.data})
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    print(request.user)
+    return Response("passed!")
