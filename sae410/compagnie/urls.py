c5c196704bea9df88085c8484d103ea3ef58c14c
Julien CLAUSS <julien.clauss@epita.fr>
2024-06-14 09:54:30 +0200

    Reservation --> Delete Implemented urls updated changing "id" to "pk" models achat and reservation updated making achat point to reservation and not the opposite

--- sae410/compagnie/urls.py
+++ sae410/compagnie/urls.py
@@ -3,11 +3,11 @@ from .views import vol_list, vol_detail, reservation_detail, reservation_list, a
 
 urlpatterns = [
     path('api/', vol_list),
-    path ('api/vol/<int:id>/', vol_detail),
+    path ('api/vol/<int:pk>/', vol_detail),
     path ('api/reservation/', reservation_list),
-    path ('api/reservation/<int:id>/', reservation_detail),
+    path ('api/reservation/<int:pk>/', reservation_detail),
     path ('api/achat/', achat_list),
-    path ('api/achat/<int:id>/', achat_detail),
+    path ('api/achat/<int:pk>/', achat_detail),
     path ('api/register/', register),
     path ('api/login/', login),
     path ('api/test_login/', test_token),
